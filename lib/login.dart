c98ca94ba7599a33d1ab0f26b9871a0bd33e34da
Hamda Fizal <hamda.fzl@gmail.com>
2021-05-02 00:50:04 +0530

    update login page

--- lib/login.dart
+++ lib/login.dart
@@ -8,22 +8,111 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  String text = 'Sign in';
+  String textAlt='Sign up';
+  String textAltAdd= 'Don\'t have an account?';
+  String buttonText='Login';
+  bool signup;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: TextButton(
-            child: Text('login'),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => AlertPage(
-                            keyword: 'hell',
-                          )));
-            },
-          ),
-        ));
+        appBar: AppBar(
+          title: Text('MayDay'),
+          backgroundColor: Colors.amber,
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/icon.png',width: 80,height : 80 ,),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      text,
+                      style: TextStyle(fontSize: 25,color: Colors.amber,fontWeight: FontWeight.bold),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'email',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                // ignore: deprecated_member_use
+
+                Container(
+                    height: 60,
+                    width : 360,
+                    padding: EdgeInsets.fromLTRB(10, 10,10, 0),
+                    // ignore: deprecated_member_use
+                    child: RaisedButton(
+                      padding: EdgeInsets.all(10),
+                      textColor: Colors.white,
+                      color: Colors.amber,
+                      child: Text(buttonText,style: TextStyle(fontSize: 20),),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(passwordController.text);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomePage(
+
+                                )));
+                      },
+                    )),
+                Container(
+                    child: Row(
+                      children: <Widget>[
+                        Text(textAltAdd),
+                        // ignore: deprecated_member_use
+                        FlatButton(
+                          textColor: Colors.indigo,
+                          child: Text(
+                            textAlt,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {
+                              setState(() {
+                                if(signup==true) {
+                                  text = 'Sign up';
+                                  textAlt = 'Sign in';
+                                  textAltAdd = 'Already have an account? ';
+                                  buttonText='Register';
+                                  signup=false;
+                                }
+                                else{
+                                  text = 'Sign in';
+                                  textAlt = 'Sign up';
+                                  textAltAdd = 'Don\'t have an account? ';
+                                  buttonText= 'Login';
+                                  signup=true;
+                                }
+                              });
+                          },
+                        )
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ))
+              ],
+            )));
   }
 }
