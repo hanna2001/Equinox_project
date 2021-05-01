b1bcc572bd2427311bc2d3adc3eb0255997e22f3
Hanna Sabu Mampallil <hanblessab@gmail.com>
2021-05-01 23:50:13 +0530

    setting keyword

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,23 +1,19 @@
-//the page to set the keyword
-import 'dart:math';
+import 'package:flutter/material.dart';
 
-// import 'package:avatar_glow/avatar_glow.dart';
-// import 'package:flutter/material.dart';
-// import 'package:highlight_text/highlight_text.dart';
-// import 'package:speech_to_text/speech_to_text.dart' as stt;
-//
-// class HomePage extends StatefulWidget {
-//   @override
-//   _HomePageState createState() => _HomePageState();
-// }
-//
-// class _HomePageState extends State<HomePage> {
-// final Map<String, HighlightedWord> _highlights = {
-//   'flutter': HighlightedWord(
-//     onTap: () => print('flutter'),
-//     textStyle: const TextStyle(
-//       color: Colors.green,
-//       fontWeight: FontWeight.bold,
-//     ),
-//   )
-// };
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(),
+        ),
+      ),
+    );
+  }
+}
